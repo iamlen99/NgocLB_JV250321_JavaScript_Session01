b8c870836dbe95c035696e2ac7355be49c8c62ef
iamlen99 <allblue2903@gmail.com>
2025-04-22 21:15:57 +0700

    Upload bài tập session 01 - 8

---
+++ exercise 08/script.js
@@ -0,0 +1,19 @@
+let numA = "5";
+let numB = "-1";
+let numC = "Hello";
+
+console.log("Convert to number: ");
+console.log(Number(numA));
+console.log(Number(numB));
+// console.log(number(numC));
+
+let num1 = 55;
+let num2 = -10;
+let num3 = 0;
+
+console.log("Convert to string: ");
+console.log(num1.toString());
+console.log(num2.toString());
+console.log(num3.toString());
+
+// console.log(`The square root of ${num} is: ` + sqrtNum);
